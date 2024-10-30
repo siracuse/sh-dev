e173d189faf10610b25f0476aa60524cdf5cdc19
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2024-10-30 12:44:15 +0100

    robots.txt and sitemap.xml

--- components/navbar.php
+++ components/navbar.php
@@ -1,3 +1,13 @@
+<!--<nav>-->
+<!--    <ul>-->
+<!--        <li><a href="/">Projets</a></li>-->
+<!--        <li><a href="blog">Blog</a></li>-->
+<!--        <li><a href="about">À propos</a></li>-->
+<!--        <li><a href="https://www.linkedin.com/in/harichandra-siracuse/" target="_blank">In</a></li>-->
+<!--    </ul>-->
+<!--</nav>-->
+
+
 <nav>
     <ul>
         <li><a href="index">Projets</a></li>
