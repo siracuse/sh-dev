d67b5c6e056b35851127ebd0a085f08d6a274124
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2024-10-30 12:05:23 +0100

    SEO color contraste

--- components/header.php
+++ components/header.php
@@ -3,7 +3,7 @@
 <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
 <!--<link rel="stylesheet" href="/assets/style.css">-->
 <!--<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>-->
-<!--<link href="/assets/img/SH.ico" rel="shortcut icon" type="image/x-icon"/>-->
+<!--<link href="/assets/img/hari-siracuse.ico" rel="shortcut icon" type="image/x-icon"/>-->
 <!--<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">-->
 
 <meta charset="UTF-8">
