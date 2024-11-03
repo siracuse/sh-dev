a259b0c83ded3b3a1cdd00ec2a4dc1953dcb31ea
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2024-11-03 11:43:11 +0100

    page 404

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php
+    include('components/header.php');
+    ?>
+    <title>Page non trouvée - sh-dev.fr</title>
+</head>
+<body class="page-404">
+
+<?php
+include('components/navbar.php');
+?>
+
+<section class="section-404">
+    <h1 class="mg-bottom-20 font-s-2em">404 - Page non trouvée</h1>
+    <p class="mg-bottom-20">Oups ! La page que vous cherchez n'existe pas.</p>
+    <a class="blog-sources-link" href="/">Retour à l'accueil</a>
+</section>
+
+<?php
+include('components/footer.php');
+?>
+</body>
+</html>
