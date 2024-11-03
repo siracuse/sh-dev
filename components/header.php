b042883e21f845f9777e3ceef0c89ebb34317595
SIRACUSE Harichandra <siracuse.harichandra@gmail.com>
2024-11-03 12:34:23 +0100

    sitemap

--- components/header.php
+++ components/header.php
@@ -1,6 +1,7 @@
 <!--<meta charset="UTF-8">-->
 <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
 <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<!--<link rel="canonical" href="https://sh-dev.fr/" />-->
 <!--<link rel="stylesheet" href="/assets/style.css">-->
 <!--<link href="/assets/img/hari-siracuse.ico" rel="shortcut icon" type="image/x-icon">-->
 <!--<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" media="print" onload="this.media='all'" rel="stylesheet">-->
@@ -8,6 +9,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="canonical" href="https://sh-dev.fr/" />
 <link rel="stylesheet" href="/sh-dev/assets/style.css">
 <link href="/sh-dev/assets/img/hari-siracuse.ico" rel="shortcut icon" type="image/x-icon">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" media="print" onload="this.media='all'" rel="stylesheet">
